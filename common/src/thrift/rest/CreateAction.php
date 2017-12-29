955018f2abb248a87957afd4a22e8588b20aa2bf
Darkgel <1627116986@qq.com>
2017-12-29 17:29:02 +0800

    1.自定义 resetful action 1.自定义TActiveRecord

---
+++ common/src/thrift/rest/CreateAction.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: michael.shi
+ * Date: 2017/12/29
+ * Time: 16:20
+ */
+class CreateAction
+{
+
+}
---
+++ common/src/thrift/rest/DeleteAction.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: michael.shi
+ * Date: 2017/12/29
+ * Time: 16:21
+ */
+class DeleteAction
+{
+
+}
